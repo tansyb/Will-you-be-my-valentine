42f96418916548a3cf42f6a54deafd37a4750a67
Tamsyn <tlin0812@uni.sydney.edu.au>
2026-04-16 10:31:51 +1000

    added buttons and fixed screen width

--- src/App.jsx
+++ src/App.jsx
@@ -21,27 +21,43 @@ function AnimatedWord({ word, startIndex }) {
   );
 }
 
+
 function App() {
   const [isOpened, setIsOpened] = useState(false);
   const text = "Will you be my Valentine?";
   const words = text.split(" ");
+  const [answered, setAnswered] = useState(false);
+
+  // random position for the No button
+  const [noPos, setNoPos] = useState({ top: "70%", left: "55%" });
+
+  // generates random pos
+  function runAway() {
+    const top = Math.random() * 80 + 5;  // 5–85%
+    const left = Math.random() * 80 + 5; // 5–85%
+    setNoPos({ top: `${top}%`, left: `${left}%` });
+  }
 
   return (
     <div className="scene">
+
+      {/* closed letter scene */}
       <div
         className="bg-layer"
         style={{ backgroundImage: `url(${bg1})` }}
-        // add in the title for open me
       ></div>
       <div
         className="opening-text"
       >Open Me</div>
 
+      {/* open letter scene */}
       <div
         className={`bg-layer bg-layer-top ${isOpened ? "opened" : ""}`}
         style={{ backgroundImage: `url(${bg2})` }}
         
       >
+
+        {/* animated text "will you be my valentine?" */}
         <div className="bg2-text">
           {words.map((word, wi) => (
             <AnimatedWord
@@ -51,10 +67,32 @@ function App() {
             />
           ))}
         </div>
+
+        {/* if yes button is press, goes to black screen */}
+        {answered && (
+          <div className="black-screen" />
+        )}
+
+        <button
+          className="btn btn-yes"
+          onClick={() => setAnswered(true)}
+        >
+          Yes!
+        </button>
+
+        {/* on hover, no button runs away to random pos */}
+        <button
+          className="btn btn-no"
+          style={{ top: noPos.top, left: noPos.left }}
+          onMouseEnter={runAway}
+          onClick={runAway}
+        >
+          No way!
+        </button>
+          {!isOpened && (
+            <div className="clickable-envelope" onClick={() => setIsOpened(true)}/>
+          )}
       </div>
-      {!isOpened && (
-        <div className="clickable-envelope" onClick={() => setIsOpened(true)}/>
-      )}
     </div>
   );
 }
