9c02d74442d75af47224bbd5cfcdd9524eeb46c4
Tamsyn <tlin0812@uni.sydney.edu.au>
2026-04-21 15:42:31 +1000

    added deployment dependencies

--- electron/main.js
+++ electron/main.js
@@ -1,8 +1,11 @@
-import { app, BrowserWindow } from 'electron'
-import { fileURLToPath } from 'url'
-import path from 'path'
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const isDev = !app.isPackaged;
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -10,24 +13,15 @@ function createWindow() {
     height: 600,
     resizable: false,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      contextIsolation: true
     }
-  })
+  });
 
-  // In dev, load Vite dev server. In production, load built files.
-  if (process.env.NODE_ENV === 'development') {
-    win.loadURL('http://localhost:5173')
+  if (isDev) {
+    win.loadURL("http://localhost:5173");
   } else {
-    win.loadFile(path.join(__dirname, '../dist/index.html'))
+    win.loadFile(path.join(__dirname, "../dist/index.html"));
   }
 }
 
-app.whenReady().then(createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
+app.whenReady().then(createWindow);
